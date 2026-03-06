5c43add504e30854df2f4d057920fc943682ae5b
francisb <byaruhangafrancis23@gmail.com>
2026-03-06 20:41:06 +0300

    Convert F to C

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -3,6 +3,7 @@ const convertToCelsius = function(fahrenheit) {
 };
 
 const convertToFahrenheit = function() {
+  return Math.round(((celsius * 9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
