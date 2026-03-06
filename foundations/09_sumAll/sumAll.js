5bbd55d789767cd1e8ce789b5430b51b420c5d8d
francisb <byaruhangafrancis23@gmail.com>
2026-03-06 19:58:40 +0300

    Determine range regardless of order

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
     if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+
+    // Determine range regardless of order
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
 };
 
 // Do not edit below this line
