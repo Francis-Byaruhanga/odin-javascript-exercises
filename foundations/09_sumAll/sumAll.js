3024f9ab6b08f395b94562183a64f7b7f36b6f2b
francisb <byaruhangafrancis23@gmail.com>
2026-03-06 20:04:23 +0300

    Loop and accumulate within range

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -4,6 +4,13 @@ const sumAll = function(a, b) {
     // Determine range regardless of order
     const min = Math.min(a, b);
     const max = Math.max(a, b);
+
+    // Loop and accumulate
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
