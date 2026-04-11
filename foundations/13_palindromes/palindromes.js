49984b5f0bc0699d48d0d122f58f0eb37a8ddace
francisb <byaruhangafrancis23@gmail.com>
2026-04-12 00:04:25 +0300

    Clean and reverse string

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
 const palindromes = function () {
+    // Remove all non-alphanumeric characters and convert to lowercase
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    // Reverse the cleaned string
+    const reversed = cleaned.split('').reverse().join('');
 };
 
 // Do not edit below this line
