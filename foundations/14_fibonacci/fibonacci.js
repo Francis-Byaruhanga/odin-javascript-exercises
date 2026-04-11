4e60587fa2161c28fc6836200caad7c14b7dcff4
francisb <byaruhangafrancis23@gmail.com>
2026-04-12 00:32:39 +0300

    Set initial values

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -3,6 +3,12 @@ const fibonacci = function(n) {
     if (n <= 0) return 0;
     if (n === 1 || n === 2) return 1;
 
+    // Start with the first two numbers
+    let previousNumber = 1;
+    let currentNumber = 1;
+
+    
+
 
 };
 
